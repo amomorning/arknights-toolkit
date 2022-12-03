988da2143881d598439202346a2710e461d79a37
Nero <286830682@qq.com>
2022-12-03 16:05:56 +0800

    Add auto_click.py

---
+++ auto_click.py
@@ -0,0 +1,30 @@
+import click
+import logging
+
+@click.group()
+def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+
+@main.command()
+@click.option('--mode',
+              type=click.Choice(['adb', 'screen'], case_sensitive=False),
+              default='adb')
+@click.option('--time-interval', default=2.0)
+def run(mode, time_interval):
+    if mode == 'adb':
+        import os
+        import time
+        while True:
+            os.popen('adb shell input tap 1650 960')
+            time.sleep(time_interval)
+    elif mode == 'screen':
+        import pyautogui
+        pyautogui.PAUSE = time_interval
+        while True:
+            pyautogui.press('c')
+
+
+if __name__ == '__main__':
+    main()
